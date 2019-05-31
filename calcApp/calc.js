7cc10b2e9ea6d8c81843f9c4ac528ed4cfa6250f
b-c-Jones <49840918+b-c-Jones@users.noreply.github.com>
2019-05-31 17:01:08 -0500

    Update calc.js

--- calcApp/calc.js
+++ calcApp/calc.js
@@ -4,200 +4,219 @@ require([
     "dojo/dom-construct",
     "dijit/form/Button",
     "dijit/Dialog",
+    "dojo/on",
+    "dojo/keys",
+    "dojo/query",
     "dojo/domReady!"
-], function (dom, ready, domConstruct, Button, Dialog) {
-    /////////////////////////////////////////////////////////////////////////////
-    for (var i = 0; i < 10; i++) {
-        domConstruct.create("button", {innerHTML: `${i}`, id: `button${i}`, class: "btn btn-large"}, "calculator")
-    }
-    domConstruct.create("button", {innerHTML: "Calculator", id: "Calculator", class: "btn"}, "calculator")
-    //////////////////////////////////////////////////////////////////////////
+], function (dom, ready, domConstruct, Button, Dialog, on, keys, query) {
+    
+    _calciteCalculator = function(title, msg, columns) {
+        if (!columns) columns=6;
+        var alertOverlay = domConstruct.create("div", { class: "modal-overlay is-active" }, document.body);
+        var alertContent = domConstruct.create("div", { class: "modal-content column-"+columns, "role":"dialog", "aria-labelledby":"modal" }, alertOverlay);
+        var alertButtons = domConstruct.create("div", { class: "text-right" }, alertContent);
+        domConstruct.create("h5", { class: "trailer-half text-blue", innerHTML: "Calculator" }, alertContent);
+        domConstruct.create("div", { class: "modalDiv" }, alertContent);
+        var okBtn = domConstruct.create("button", { class: "btn btn-small", innerHTML: "X" }, alertButtons);
+        on(okBtn, "click", function() { dojo.destroy(alertOverlay) });
+    };
+    _calciteCalculator("test", "testing");
     
     calcDialog = new Dialog({
         title: "Calculator",
-        style: `width: 250px; height: 380px`,
+        style: "width: 256px; height: 415px",
+        //class: "modal-content",
         content: `<span id="calcApp"></span>`
     });
 
-    new Button({
-        label: "Calculator",
-        id: "calcBtn",
-        class: "btn",
-        onClick: function(){
-            calcDialog.show();
-        }
-    }, "Calculator").startup();
-
-    domConstruct.create("h3", { innerHTML: "Problem", id: "input" }, "calcApp");
-    domConstruct.create("h3", { innerHTML: "Solution", id: "output" }, "calcApp");
-    var solution = dom.byId("output");
+    on(domConstruct.create("div", { "innerHTML": "Calculator", "class": "btn" }, "calculator"), 'click', function(){
+        calcDialog.show();
+        _calciteCalculator("test", "testing");
+     });
+    
+    domConstruct.create("textarea", { innerHTML: "Problem", id: "input", rows: "6", disabled: "" }, "calcApp");
+    domConstruct.create("textarea", { innerHTML: "Solution", id: "output", rows: "1", disabled: "" }, "calcApp");
     var problem = dom.byId("input");
+    var solution = dom.byId("output");
     var problemArr = [""];
     var difference;
     // creates the numbered calculator buttons
     for (var i = 0; i < 10; i++) {
-        new Button({
-            label: i,
-            id: "button-" + i,
-            class: "calcButton",
-            onClick: function () {
-                if (problemArr.slice(-1) != ")") {
-                    problemArr.push(this.label);
-                    // join problemArr with no separation then split it along the operators
-                    problemArr = problemArr.join('').split(/(?<= \D )|(?= \D )/);
-                    // get rid of all leading zeros
-                    for (var j = 0; j < problemArr.length; j++) {
-                        problemArr[j] = problemArr[j].replace(/^0+(?=\d)/, '');
-                    };
+        on(domConstruct.create("div", { "innerHTML": i, "id": "button-" + i, "class": "btn calcButton" }, "calcApp"), 'click', function(){
+            numClick(this.innerHTML);
+         });
+    };
+
+    on(domConstruct.create("div", { "innerHTML": "+", "id": "plus-btn", "class": "btn calcButton" }, "calcApp"), 'click', function(){
+        operatorClick(this.innerHTML);
+    });
+
+    on(domConstruct.create("div", { "innerHTML": "-", "id": "minus-btn", "class": "btn calcButton" }, "calcApp"), 'click', function(){
+        minusClick();
+    });
+
+    on(domConstruct.create("div", { "innerHTML": "*", "id": "times-btn", "class": "btn calcButton" }, "calcApp"), 'click', function(){
+       operatorClick(this.innerHTML);
+    });
+
+    on(domConstruct.create("div", { "innerHTML": "/", "id": "divide-btn", "class": "btn calcButton" }, "calcApp"), 'click', function(){
+        operatorClick(this.innerHTML);
+     });
+
+    on(domConstruct.create("div", { "innerHTML": ".", "id": "decimal-btn", "class": "btn calcButton" }, "calcApp"), 'click', function(){
+        decimalClick();
+     });
+
+    on(domConstruct.create("div", { "innerHTML": "=", "id": "equals-btn", "class": "btn calcButton" }, "calcApp"), 'click', function(){
+        equalsClick();
+    });
+
+    on(domConstruct.create("div", { "innerHTML": "(", "id": "left-parenth-btn", "class": "btn calcButton" }, "calcApp"), "click", function(){
+        leftParenthClick();
+    });
+
+    on(domConstruct.create("div", { "innerHTML": ")", "id": "right-parenth-btn", "class": "btn calcButton" }, "calcApp"), "click", function(){
+        checkParentheses();
+        rightParenthClick();
+    });
+    
+    on(domConstruct.create("div", { "innerHTML": "⌫", "id": "backspace-btn", "class": "btn calcButton" }, "calcApp"), "click", function(){
+        backspaceClick();
+    });
+    
+    on(domConstruct.create("div", { "innerHTML": "C", "id": "clear-btn", "class": "btn calcButton" }, "calcApp"), "click", function(){
+        clearClick();
+    });
+    
+    on(document,"keydown", function(event) {
+        switch(event.key) {
+            case "1":
+            case "2":
+            case "3":
+            case "4":
+            case "5":
+            case "6":
+            case "7":
+            case "8":
+            case "9":
+            case "0":
+                numClick(event.key);
+                break;
+            case "(":
+                leftParenthClick();
+                break;
+            case ")":
+                rightParenthClick();
+                break;
+            case "Backspace":
+                backspaceClick();
+                break;
+            case "c":
+                clearClick();
+                break;
+            case "+":
+                operatorClick("+");
+                break;
+            case "-":
+                minusClick();
+                break;
+            case "*":
+                operatorClick("*");
+                break;
+            case "/":
+                operatorClick("/");
+                break;
+            case "=":
+            case "Enter":
+                equalsClick();
+                break;
+            case ".":
+                decimalClick();
+                break;
+            default:
+                console.log("not a valid keypress")
+        };
+    });
+
+    numClick = function (innerHTML) {
+        if (problemArr.slice(-1) != ")") {
+            problemArr.push(innerHTML);
+            // join problemArr with no separation then split it along the operators
+            problemArr = problemArr.join('').split(/(?<= \D )|(?= \D )/);
+            // get rid of all leading zeros
+            for (var j = 0; j < problemArr.length; j++) {
+                problemArr[j] = problemArr[j].replace(/^0+(?=\d)/, '');
+            };
+            problem.innerHTML = problemArr.join("");
+            problem.scrollTop = problem.scrollHeight;
+        };
+    };
+    minusClick = function() {
+        if (problemArr.join("").split(/(?<= \D )|(?= \D )/).slice(-1) != " - " &&
+        problemArr.join("").split("").slice(-1) != ".") {
+            problemArr.push(" - ");
+            problem.innerHTML = problemArr.join("");
+            problem.scrollTop = problem.scrollHeight;
+        };
+    };
+    decimalClick = function() {
+        if (!problemArr[problemArr.length - 1].includes(".") && problemArr.slice(-1) != ")") {
+            problemArr.push(".");
+            problem.innerHTML = problemArr.join("");
+            problem.scrollTop = problem.scrollHeight;
+        };
+    };
+    equalsClick = function() {
+        if (problemArr[0] !== "") {
+            evalCheck();
+            checkParentheses();
+            if (difference > 0) {
+                for (var i = 0; i < difference; i++) {
+                    problemArr.push(")");
                     problem.innerHTML = problemArr.join("");
                 };
-            }
-        }).placeAt("calcApp").startup();
-    }
-
-    var buttonPlus = new Button({
-        label: "+",
-        id: "plus-btn",
-        class: "calcButton",
-        onClick: function () {
-            console.log(problemArr)
-            checkOperator(this.label);
-        }
-    }).placeAt("calcApp").startup();
-
-    var buttonMinus = new Button({
-        label: "-",
-        id: "minus-btn",
-        class: "calcButton",
-        onClick: function () {
-            console.log(problemArr)
-            if (problemArr.length === 1 && problemArr[0] === "") {
-                problemArr = [" - "];
-                problem.innerHTML = problemArr.join("");
-            } else if (!problemArr.length){
-                problemArr = [" - "];
-                problem.innerHTML = problemArr.join("");
-            } else if (problemArr.length === 1 && problemArr[0] === " - "){
-                return false;
-            } else {
-                checkOperator(this.label);
             };
-        }
-    }).placeAt("calcApp").startup();
-
-    var buttonTimes = new Button({
-        label: "*",
-        id: "times-btn",
-        class: "calcButton",
-        onClick: function () {
-            checkOperator(this.label);
-        }
-    }).placeAt("calcApp").startup();
-
-    var buttonDivide = new Button({
-        label: "/",
-        id: "divide-btn",
-        class: "calcButton",
-        onClick: function () {
-            checkOperator(this.label);
-        }
-    }).placeAt("calcApp").startup();
-
-    var buttonDecimal = new Button({
-        label: ".",
-        id: "decimal-btn",
-        class: "calcButton",
-        onClick: function () {
-            // if the current number does not contain a decimal point and the last character is not a closing 
-            // parentheses, add a decimal point.
-            if (!problemArr[problemArr.length - 1].includes(".") && problemArr.slice(-1) != ")") {
-                problemArr.push(`${this.label}`);
-                problem.innerHTML = problemArr.join("");
-            };
-        }
-    }).placeAt("calcApp").startup();
-
-    var buttonEquals = new Button({
-        label: "=",
-        id: "equals-btn",
-        class: "calcButton",
-        // evaluate the equation and display it in solution.innerHTML rounded to 5 decimal places
-        onClick: function () {
-            if (problemArr[0] !== "") {
-                evalCheck();
-                checkParentheses();
-                if (difference > 0) {
-                    for (var i = 0; i < difference; i++) {
-                        problemArr.push(")");
-                        problem.innerHTML = problemArr.join("");
-                    };
-                };
-                answer = eval(problem.innerHTML);
-                solution.innerHTML = +answer.toFixed(5);
-            };
-        }
-    }).placeAt("calcApp").startup();
-
-    var leftParenth = new Button({
-        label: "(",
-        id: "left-parenth-btn",
-        class: "calcButton",
-        onClick: function () {
-            if (problemArr.slice(-1) == " + " ||
-                problemArr.slice(-1) == " - " ||
-                problemArr.slice(-1) == " * " ||
-                problemArr.slice(-1) == " / " ||
-                problemArr.slice(-1) == "") {
-                problemArr.push(`${this.label}`);
-                problem.innerHTML = problemArr.join("");
-            };
-        }
-    }).placeAt("calcApp").startup();
-
-    var rightParenth = new Button({
-        label: ")",
-        id: "right-parenth-btn",
-        class: "calcButton",
-        onClick: function () {
-            checkParentheses();
-            if (difference > 0 && problemArr.join("").slice(-1) != "(" && problemArr.join("").slice(-1) != "." && !checkIfOperator()) {
-                problemArr.push(`${this.label}`);
-                problem.innerHTML = problemArr.join("");
-            };
-        }
-    }).placeAt("calcApp").startup();
-
-    var backspace = new Button({
-        label: "⌫",
-        id: "backspace-btn",
-        class: "calcButton",
-        onClick: function () {
-            console.log(problemArr)
-            if (checkIfOperator()) {
-                problemArr = problemArr.slice(0, -1);
-            } else {
-                problemArr = problemArr.join("").split("").slice(0, -1).join("").split(/(?<= \D )|(?= \D )/);
-            };
-            if (problemArr[0] == "" || !problemArr.length) {
-                problem.innerHTML = "Problem";
-            } else {
-                problem.innerHTML = problemArr.join("");
-            };
-        }
-    }).placeAt("calcApp").startup();
-
-    var clear = new Button({
-        label: "C",
-        id: "clear-btn",
-        class: "calcButton",
-        onClick: function () {
-            problemArr = [""];
+            answer = eval(problem.innerHTML);
+            solution.innerHTML = +answer.toFixed(5);
+        };
+    };
+    leftParenthClick = function() {
+        if (problemArr.slice(-1) == " + " ||
+        problemArr.slice(-1) == " - " ||
+        problemArr.slice(-1) == " * " ||
+        problemArr.slice(-1) == " / " ||
+        problemArr.slice(-1) == "") {
+            problemArr.push("(");
+            problem.innerHTML = problemArr.join("");
+            problem.scrollTop = problem.scrollHeight;
+        };
+    };
+    rightParenthClick = function() {
+        if (difference > 0 && problemArr.join("").slice(-1) != "(" && problemArr.join("").slice(-1) != "." && !checkIfOperator()) {
+            problemArr.push(")");
+            problem.innerHTML = problemArr.join("");
+            problem.scrollTop = problem.scrollHeight;
+        };
+    };
+    backspaceClick = function() {
+        if (checkIfOperator()) {
+            problemArr = problemArr.slice(0, -1);
+        } else {
+            problemArr = problemArr.join("").split("").slice(0, -1).join("").split(/(?<= \D )|(?= \D )/);
+        };
+        if (problemArr[0] == "" || !problemArr.length) {
             problem.innerHTML = "Problem";
-            solution.innerHTML = "Solution";
-        }
-    }).placeAt("calcApp").startup();
+        } else {
+            problem.innerHTML = problemArr.join("");
+        };
+    };
+    clearClick = function() {
+        problemArr = [""];
+        problem.innerHTML = "Problem";
+        solution.innerHTML = "Solution";
+    };
 
-    checkOperator = function (label) {
+    operatorClick = function (innerHTML) {
         if (problemArr.join("").split(/(?<= \D )|(?= \D )/).slice(-1) == " + " ||
             problemArr.join("").split(/(?<= \D )|(?= \D )/).slice(-1) == " - " ||
             problemArr.join("").split(/(?<= \D )|(?= \D )/).slice(-1) == " * " ||
@@ -205,21 +224,21 @@ require([
             problemArr.join("").split("").slice(-1) == "(" ||
             problemArr.join("").split("").slice(-1) == ".") {
                 problemArr = problemArr.slice(0, -1);
-                console.log(problemArr)
-                checkOperator(label);
+                operatorClick(innerHTML);
             } else if(problemArr[0] === "" || !problemArr.length){
                 problem.innerHTML = "Problem";
                 problemArr = [""];
             } else {
-                problemArr.push(` ${label} `);
+                problemArr.push(` ${innerHTML} `);
                 problem.innerHTML = problemArr.join("");
+                problem.scrollTop = problem.scrollHeight;
             };
-    }
+    };
     checkParentheses = function () {
         var leftCounter = problemArr.join("").replace(/[^\(]/g, "").length;
         var rightCounter = problemArr.join("").replace(/[^\)]/g, "").length;
         return difference = leftCounter - rightCounter;
-    }
+    };
     checkIfOperator = function () {
         if (problemArr.slice(-1) == " + " ||
             problemArr.slice(-1) == " - " ||
@@ -229,7 +248,7 @@ require([
         } else {
             return false;
         };
-    }
+    };
     evalCheck = function() {
         if (problemArr.join("").split("").slice(-1) == "(" ||
         problemArr.join("").split("").slice(-1) == "." ||
@@ -243,6 +262,5 @@ require([
         } else {
             problem.innerHTML = "Problem";
         }
-        
-    }
+    };
 });
